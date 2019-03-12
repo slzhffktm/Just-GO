24297f3b6e7f6fed4c95580d3c499e8702efbaeb
Virmith <13516049@std.stei.itb.ac.id>
2019-03-12 22:10:35 +0700

    Initial animation

---
+++ Just GO!/Assets/Script/Animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+    }
+}
