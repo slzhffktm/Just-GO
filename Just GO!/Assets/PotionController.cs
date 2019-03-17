b81a21a33a8d37f902e0988b1d2e8af76c724fbc
slzhffktm <13516121@std.stei.itb.ac.id>
2019-03-17 16:39:05 +0700

    Create potion controller for potion movement

---
+++ Just GO!/Assets/PotionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector2 moveDirection = Vector2.zero;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDown();
+    }
+
+    private void MoveDown()
+    {
+        moveDirection.y -= 50 * Time.smoothDeltaTime;
+        controller.Move(moveDirection * Time.smoothDeltaTime);
+    }
+}
