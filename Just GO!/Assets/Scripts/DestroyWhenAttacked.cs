0026fbeaad34852d7747ab5e733561db27cd8a70
Ellen Nugroho <ellennugroho9@gmail.com>
2019-03-17 15:21:07 +0700

    player and enemy collision

---
+++ Just GO!/Assets/Scripts/DestroyWhenAttacked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenAttacked : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Attack")
+        {
+            Destroy(gameObject);
+        } else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Just GO!/Assets/Scripts/PlayerController.cs
+++ Just GO!/Assets/Scripts/PlayerController.cs
@@ -16,22 +16,30 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector2 moveDirection = Vector2.zero;
 
+    private bool life = true;
+
+    Collider mCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = transform.GetComponent<Rigidbody2D>();
         playerAnimator = gameObject.GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        mCollider = GetComponent<Collider>();
     }
 
     void Update()
     {
-        if (controller.isGrounded)
+        if (true)
         {
-            jumpCount = 0;
+            if (controller.isGrounded)
+            {
+                jumpCount = 0;
+            }
+            MovePlayer();
+            playerCamera.transform.position = new Vector3(transform.position.x + 4, transform.position.y, playerCamera.transform.position.z);
         }
-        MovePlayer();
-        playerCamera.transform.position = new Vector3(transform.position.x + 4, transform.position.y, playerCamera.transform.position.z);
     }
 
     private void MovePlayer()
@@ -54,5 +62,23 @@ public class PlayerController : MonoBehaviour
     private void Attack()
     {
         playerAnimator.SetTrigger("Attack");
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // NOTE: bitwise NOT is the tild character: ~
+        CollisionFlags ignoreGround = ~CollisionFlags.Below;
+
+        CollisionFlags newCollisionFlags = controller.collisionFlags & ignoreGround;
+
+        if (newCollisionFlags != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            Destroy(gameObject);
+            print("touched something OTHER than the ground");
+        }
     }
 }
